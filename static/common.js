325dcdfe9513b86c5f472b6457c6b6eba8a3dd94
Badel2 <2badel2@gmail.com>
2018-09-25 23:07:24 +0200

    Scroll support for mobile

--- static/common.js
+++ static/common.js
@@ -79,7 +79,12 @@ window.onload = function () {
     center_butt.onclick = function() {
         var x = document.getElementById('center_x').value;
         var z = document.getElementById('center_z').value;
-        Game.centerAt(x, z);
+        // Center at block if supported, otherwise center at chunk/fragment
+        if(Game.centerAtBlock) {
+            Game.centerAtBlock(x, z);
+        } else {
+            Game.centerAt(x, z);
+        }
     };
     var elem = document.getElementById('demo'),
     elemLeft = elem.offsetLeft,
@@ -89,48 +94,109 @@ window.onload = function () {
     var dragging = null;
 
     // Add event listener for `click` events.
-    elem.addEventListener('mousedown', function(event) {
-        var x = event.pageX - elemLeft,
-            y = event.pageY - elemTop;
+    // TODO: touchstart for mobile support
+    // https://stackoverflow.com/a/16284281
+    var pointerEventToXY = function(e){
+      var out = {x:0, y:0};
+      if(e.type == 'touchstart' || e.type == 'touchmove' || e.type == 'touchend' || e.type == 'touchcancel'){
+        var touch = e.touches[0] || e.changedTouches[0];
+        out.x = touch.pageX;
+        out.y = touch.pageY;
+      } else if (e.type == 'mousedown' || e.type == 'mouseup' || e.type == 'mousemove' || e.type == 'mouseover'|| e.type=='mouseout' || e.type=='mouseenter' || e.type=='mouseleave') {
+        out.x = e.pageX;
+        out.y = e.pageY;
+      }
+      return out;
+    };
 
-        dragging = {x: x, y: y, initialX: x, initialY: y, actuallyScrolling: false};
-    }, false);
-    elem.addEventListener('mousemove', function(event) {
-        var x = event.pageX - elemLeft,
-            y = event.pageY - elemTop;
-        var txty = Game.mouse_coords_to_game_coords_float(x, y);
-        var tx = txty[0];
-        var ty = txty[1];
-        pos_div.innerHTML = "Chunk x: " + Math.floor(tx) + ", z: " + Math.floor(ty);
-        pos_div.innerHTML += " --- Block x: " + Math.floor(tx*16) + ", z: " + Math.floor(ty*16);
-
-        if (dragging) {
-            if (dragging.actuallyScrolling == false && (Math.abs(dragging.x - x) > 10 || Math.abs(dragging.y - y) > 10)) {
-                dragging.actuallyScrolling = true;
+    ['touchstart', 'mousedown'].forEach(function(n) {
+        elem.addEventListener(n, function(e) {
+            //console.log('elem mousedown');
+            if (n == 'touchstart') {
+                e.preventDefault();
             }
-            if (dragging.actuallyScrolling) {
-                Game.scrollBy(dragging.x - x, dragging.y - y);
-                dragging.x = x;
-                dragging.y = y;
+            var pointer = pointerEventToXY(e);
+            var x = pointer.x - elemLeft,
+                y = pointer.y - elemTop;
+
+            dragging = {x: x, y: y, actuallyScrolling: false};
+        }, false)
+    });
+    ['touchmove', 'mousemove'].forEach(function(n) {
+        elem.addEventListener(n, function(e) {
+            //console.log('elem mousemove');
+            var pointer = pointerEventToXY(e);
+            var x = pointer.x - elemLeft,
+                y = pointer.y - elemTop;
+            var txty = Game.mouse_coords_to_game_coords_float(x, y);
+            var tx = txty[0];
+            var ty = txty[1];
+            if (map.getFragment) {
+                pos_div.innerHTML = "Fragment x: " + Math.floor(tx) + ", z: " + Math.floor(ty);
+                pos_div.innerHTML += " --- Block x: " + Math.floor(tx*FRAG_SIZE) + ", z: " + Math.floor(ty*FRAG_SIZE);
+            } else {
+                pos_div.innerHTML = "Chunk x: " + Math.floor(tx) + ", z: " + Math.floor(ty);
+                pos_div.innerHTML += " --- Block x: " + Math.floor(tx*16) + ", z: " + Math.floor(ty*16);
             }
-        }
-    }, false);
-    elem.addEventListener('mouseup', function(event) {
-        var x = event.pageX - elemLeft,
-            y = event.pageY - elemTop;
+        }, false)
+    });
 
-        if (dragging.actuallyScrolling == false) {
-            Game.clickTile(x, y);
+    ['touchmove', 'mousemove'].forEach(function(n) {
+        window.addEventListener(n, function(e) {
+            if (dragging) {
+                var pointer = pointerEventToXY(e);
+                var x = pointer.x - elemLeft,
+                    y = pointer.y - elemTop;
+                if (dragging.actuallyScrolling == false && (Math.abs(dragging.x - x) > 10 || Math.abs(dragging.y - y) > 10)) {
+                    // Moving more than 10 pixels from the initial position starts the scrolling
+                    dragging.actuallyScrolling = true;
+                }
+                if (dragging.actuallyScrolling) {
+                    Game.scrollBy(dragging.x - x, dragging.y - y);
+                    dragging.x = x;
+                    dragging.y = y;
+                }
+            }
+        }, false)
+    });
 
-            // Update selection textarea
-            var seltextarea = document.getElementById('selection_output');
-            seltextarea.value = stringify({
-                slime_chunks: Game.getSelection(0, 1),
-                slime_chunks_not: Game.getSelection(0, 2)
-            }, { maxLength: 20 });
-        }
-        dragging = null;
-    }, false);
+    ['touchend', 'mouseup'].forEach(function(n) {
+        elem.addEventListener(n, function(e) {
+            //console.log('elem mouseup');
+            if (n == 'touchend') {
+                e.preventDefault();
+            }
+            var pointer = pointerEventToXY(e);
+            var x = pointer.x - elemLeft,
+                y = pointer.y - elemTop;
+
+            if (dragging == null) {
+                // The window event handler was executed first, gg
+                console.error('BUG: The window event handler was executed before the elem event handler for event mouseup');
+            }
+            if (dragging && dragging.actuallyScrolling == false) {
+                Game.clickTile(x, y);
+
+                // Update selection textarea
+                var seltextarea = document.getElementById('selection_output');
+                seltextarea.value = stringify({
+                    slime_chunks: Game.getSelection(0, 1),
+                    slime_chunks_not: Game.getSelection(0, 2)
+                }, { maxLength: 20 });
+            }
+            dragging = null;
+        }, false)
+    });
+
+    ['touchend', 'mouseup'].forEach(function(n) {
+        window.addEventListener(n, function(e) {
+            if (n == 'touchend') {
+                // This breaks the page
+                //e.preventDefault();
+            }
+            dragging = null;
+        }, false)
+    });
 
     // Update selection textarea
     var seltextarea = document.getElementById('selection_output');
