0c027e1b96e32a2482371332cb1aa855c4f196c7
Badel2 <2badel2@gmail.com>
2018-09-26 06:29:41 +0200

    Oh yeah, the worker

---
+++ static/worker_b.js
@@ -0,0 +1,33 @@
+importScripts("wasm_gui.js");
+
+// Simulate lengthy calculation or an async call
+function doCalculation(wasmgui, data, cb) {
+    var err = null;
+    /*
+    console.log('Message received from main script');
+    console.log(data);
+    */
+    var fx = data.fx;
+    var fy = data.fy;
+    var seed = data.seed;
+    var FRAG_SIZE = data.FRAG_SIZE;
+    var rvec = wasmgui.generate_fragment(fx, fy, seed, FRAG_SIZE);
+    var result = { rvec: rvec };
+    cb(err, result);
+}
+
+// Handle incoming messages
+self.onmessage = function(msg) {
+  const {id, payload} = msg.data
+
+  Rust.wasm_gui.then( function( wasmgui ) {
+      doCalculation(wasmgui, payload, function(err, result) {
+        const msg = {
+          id,
+          err,
+          payload: result
+        }
+        self.postMessage(msg)
+      });
+  });
+}
