0cf597bc0354bde96f1eac61222824d85aae6c7f
Badel2 <2badel2@gmail.com>
2018-09-27 00:47:52 +0200

    Fix build command (feature name), download cargo web binary

---
+++ ci/README.txt
@@ -0,0 +1,4 @@
+The scripts in this directory are meant to be run from the root project dir:
+
+ci/script.sh
+
---
+++ ci/build_demo.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cargo +nightly web build --target=wasm32-unknown-unknown --bin wasm_gui --features="wasm"
+cp target/wasm32-unknown-unknown/release/wasm_gui.* static/
+cp -rf static target/deploy
+cargo doc --all-features
+cp -rf target/doc target/deploy/doc
