ab6390ebe1e8931b9587feb5c4457da4d59ac42c
Badel2 <2badel2@gmail.com>
2019-02-20 23:33:56 +0100

    Build in release mode

--- ci/build_demo.sh
+++ ci/build_demo.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cargo +nightly web build --target=wasm32-unknown-unknown --bin wasm_gui --features="wasm"
+cargo +nightly web build --target=wasm32-unknown-unknown --bin wasm_gui --features="wasm" --release
 cp target/wasm32-unknown-unknown/release/wasm_gui.* static/
 cp -rf static target/deploy
 cargo doc --all-features
