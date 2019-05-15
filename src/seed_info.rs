492f06a249c7675b8f0d37b62209eb2d14f09f34
Badel2 <2badel2@gmail.com>
2019-05-16 00:11:52 +0200

    Change seedInfo biomes field to HashMap<BiomeId, Vec<Point>>
    Because in JSON it is easier to handle maps with string keys, an array key like [1, -2] would have to be converted to string. For example, to indicate that the biome at x:1, z:-2 is river (id 7), we would do: {"1,-2": 7}. Using the new way we just convert the biomeId into a string: {"7": [[1, -2]]}

--- src/seed_info.rs
+++ src/seed_info.rs
@@ -1,13 +1,15 @@
 use crate::chunk::Chunk;
+use crate::biome_layers::Area;
+use crate::biome_layers::Map;
 use std::collections::HashMap;
 use serde::{Deserialize, Deserializer, Serialize, Serializer };
 use serde_derive::{Deserialize, Serialize};
 use serde_json;
 
 // TODO: use real types
-type Point = (i64, i64);
-type BiomeId = i32;
-type MinecraftVersion = String;
+pub type Point = (i64, i64);
+pub type BiomeId = i32;
+pub type MinecraftVersion = String;
 
 #[derive(Debug, Default, PartialEq, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
@@ -34,7 +36,7 @@ pub struct SeedStructures {
 pub struct SeedInfo {
     pub version: MinecraftVersion,
     #[serde(default, skip_serializing_if = "is_default")]
-    pub biomes: HashMap<Point, BiomeId>,
+    pub biomes: HashMap<BiomeId, Vec<Point>>,
     #[serde(flatten)]
     pub positive: SeedStructures,
     // Coords of structures that do not exist, useful to remove duplicates
@@ -53,6 +55,46 @@ impl SeedInfo {
 
         Ok(seed_info)
     }
+}
+
+fn area_from_coords<'a, I>(c: I) -> Area
+where
+    I: IntoIterator<Item = &'a Point>
+{
+    let mut c = c.into_iter();
+    let c0 = c.next();
+    if c0.is_none() {
+        // On empty coords, return empty area
+        return Area { x: 0, z: 0, w: 0, h: 0 }
+    }
+
+    let c0 = c0.unwrap();
+    let (mut x_min, mut z_min) = c0;
+    let (mut x_max, mut z_max) = c0;
+
+    for &(x, z) in c {
+        use std::cmp::{min, max};
+        x_min = min(x_min, x);
+        z_min = min(z_min, z);
+        x_max = max(x_max, x);
+        z_max = max(z_max, z);
+    }
+
+    let area = Area { x: x_min, z: z_min, w: (x_max - x_min + 1) as u64, h: (z_max - z_min + 1) as u64 };
+    area
+}
+
+pub fn biomes_to_map<I>(biomes: I) -> Map
+where
+    I: IntoIterator<Item = (BiomeId, Vec<Point>)>
+{
+    let h: Vec<_> = biomes.into_iter().flat_map(|(k, v)| v.into_iter().map(move |x| (x, k))).collect();
+    let area = area_from_coords(h.iter().map(|x| &x.0));
+    let mut m = Map::new(area);
+    for ((x, z), biome_id) in h {
+        m.a[((x - area.x) as usize, (z - area.z) as usize)] = biome_id;
+    }
+    m
 }
 
 #[derive(Debug)]
@@ -146,5 +188,19 @@ mod tests {
         assert_eq!(seed_info.version, "1.7".to_string());
         assert_eq!(seed_info.positive.slime_chunks, vec![Chunk { x: 14, z: -8 }, Chunk { x: 15, z: -8 }]);
         assert_eq!(seed_info.negative.slime_chunks, vec![Chunk { x: 16, z: -8 }]);
+    }
+
+    #[test]
+    fn biome_map() {
+        let json = r#"{
+            "version": "1.7",
+            "biomes": {
+                "7": [[0, 0], [2, 2]]
+            }
+        }"#;
+
+        let seed_info: SeedInfo = serde_json::from_str(json).unwrap();
+        assert_eq!(seed_info.version, "1.7".to_string());
+        assert_eq!(seed_info.biomes[&7], vec![(0, 0), (2, 2)]);
     }
 }
