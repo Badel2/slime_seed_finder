5fff7b433e33f3370eb7ca046441caca00adb64f
Badel2 <2badel2@gmail.com>
2019-11-10 21:13:29 +0100

    Set panic hook in wasm_gui
    This prints the backtrace to the web console

--- src/wasm_gui.rs
+++ src/wasm_gui.rs
@@ -22,8 +22,30 @@ use slime_seed_finder::mc_rng::McRng;
 use slime_seed_finder::seed_info::MinecraftVersion;
 use slime_seed_finder::seed_info::SeedInfo;
 
+use std::panic;
+
+//use stdweb::print_error_panic;
+// https://github.com/koute/stdweb/blob/4d337ee9a0a4542ea5803b46b5124d9bc166dcb7/src/webcore/promise_future.rs#L127
+/// Prints an error to the console and then panics.
+///
+/// If you're using Futures, it's more convenient to use [`unwrap_future`](fn.unwrap_future.html) instead.
+///
+/// # Panics
+/// This function *always* panics.
+#[inline]
+pub fn print_error_panic< A: stdweb::JsSerialize >( value: A ) -> ! {
+    js! { @(no_return)
+        console.error( @{value} );
+    }
+    panic!();
+}
+
 #[cfg(feature = "wasm")]
 fn main(){
+    // Set panic hook so we get backtrace in console
+    panic::set_hook(Box::new(|info| {
+        print_error_panic(&info.to_string());
+    }));
     // Init console logger
     stdweb_logger::Logger::init_with_level(::log::LevelFilter::Debug);
     // Don't start, wait for user to press button
