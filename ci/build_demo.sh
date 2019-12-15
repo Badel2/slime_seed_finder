dd9d9d0db30e67e0d3e6230eefac1a12c794d717
Badel2 <2badel2@gmail.com>
2019-12-16 00:42:18 +0100

    Disable deployment of documentation

--- ci/build_demo.sh
+++ ci/build_demo.sh
@@ -2,5 +2,6 @@
 cargo web build --target=wasm32-unknown-unknown --release -p slime_seed_finder_web
 cp target/wasm32-unknown-unknown/release/slime_seed_finder_web.* static/
 cp -rf static target/deploy
-cargo doc --features="main"
-cp -rf target/doc target/deploy/doc
+# Generate local documentation
+#cargo doc --features="main"
+#cp -rf target/doc target/deploy/doc
