Badel2/slime_seed_finder

commit fed526f0dacd39ede963824a1269597690ec03a0

Move wasm code to separate folder

static/anvil.js
@@ -96,8 +96,8 @@ function runGui() {
     let seltextarea = document.getElementById("selection_output");
     if (window.Worker) {
         let maxWorkers = navigator.hardwareConcurrency || 4;
-        Rust.wasm_gui.then(function(wasmgui) {
-            let seedInfo_str = wasmgui.anvil_region_to_river_seed_finder(
+        Rust.wasm_gui.then(function(slime_seed_finder_web) {
+            let seedInfo_str = slime_seed_finder_web.anvil_region_to_river_seed_finder(
                 region
             );
             console.log("Got seedInfo from wasm:");
@@ -116,9 +116,9 @@ function runGui() {
 // Count candidates
 function countCandidates() {
     Rust.wasm_gui.then(
-        function(wasmgui) {
+        function(slime_seed_finder_web) {
             let outta = document.getElementById("num_candidates");
-            outta.value = wasmgui.count_rivers(
+            outta.value = slime_seed_finder_web.count_rivers(
                 JSON.stringify({ seedInfo: seedInfo })
             );
         },

static/biomes.html
@@ -428,15 +428,15 @@
                     */
                     // Ok, I'll just implement it in Rust
                     return Rust.wasm_gui.then(
-                        function(wasmgui) {
+                        function(slime_seed_finder_web) {
                             let new_value;
                             if (add_not_sub) {
-                                new_value = wasmgui.add_2_n(
+                                new_value = slime_seed_finder_web.add_2_n(
                                     ws.value,
                                     n.value | 0
                                 );
                             } else {
-                                new_value = wasmgui.sub_2_n(
+                                new_value = slime_seed_finder_web.sub_2_n(
                                     ws.value,
                                     n.value | 0
                                 );
@@ -454,8 +454,8 @@
                 if (ws) {
                     // Ok, I'll just implement it in Rust
                     return Rust.wasm_gui.then(
-                        function(wasmgui) {
-                            let new_value = wasmgui.similar_biome_seed(
+                        function(slime_seed_finder_web) {
+                            let new_value = slime_seed_finder_web.similar_biome_seed(
                                 ws.value
                             );
                             setSeed(new_value);
@@ -612,7 +612,7 @@
                         });
                 } else {
                     return Rust.wasm_gui.then(
-                        function(wasmgui) {
+                        function(slime_seed_finder_web) {
                             let ws = document.getElementById("worldSeed");
                             // Since js doesnt support 64 bit ints, we just pass
                             // the string to the Rust code, it will be parsed there
@@ -621,7 +621,7 @@
                             console.log(
                                 "Generating fragment: " + fx + ", " + fy
                             );
-                            let rvec = wasmgui.generate_fragment(
+                            let rvec = slime_seed_finder_web.generate_fragment(
                                 fx,
                                 fy,
                                 seed,

static/index.html
@@ -110,9 +110,11 @@ Found 64-bit seeds will appear here.</textarea
                     };
                 } else {
                     Rust.wasm_gui.then(
-                        function(wasmgui) {
+                        function(slime_seed_finder_web) {
                             // TODO: check if a thread is already running and kill it
-                            outta.value = wasmgui.slime_seed_finder(seedInfo);
+                            outta.value = slime_seed_finder_web.slime_seed_finder(
+                                seedInfo
+                            );
                         },
                         function(err) {
                             console.error(err);
@@ -124,13 +126,13 @@ Found 64-bit seeds will appear here.</textarea
             // Count candidates
             function countCandidates() {
                 Rust.wasm_gui.then(
-                    function(wasmgui) {
+                    function(slime_seed_finder_web) {
                         let outta = document.getElementById("num_candidates");
                         let seltextarea = document.getElementById(
                             "selection_output"
                         );
                         let seedInfo = JSON.parse(seltextarea.value);
-                        outta.value = wasmgui.count_candidates({
+                        outta.value = slime_seed_finder_web.count_candidates({
                             seedInfo: seedInfo,
                         });
                     },
@@ -142,11 +144,13 @@ Found 64-bit seeds will appear here.</textarea
 
             // Extend 48
             Rust.wasm_gui.then(
-                function(wasmgui) {
+                function(slime_seed_finder_web) {
                     let seed48 = document.getElementById("seed48");
                     let outta = document.getElementById("output_extend48");
                     seed48.addEventListener("keyup", function(event) {
-                        outta.value = wasmgui.extend48(seed48.value);
+                        outta.value = slime_seed_finder_web.extend48(
+                            seed48.value
+                        );
                     });
                 },
                 function(err) {

static/rivers.js
@@ -337,13 +337,13 @@ function runWorkers(numWorkers, seedInfo) {
             );
             // Draw sample image to canvas
             if (l42AreaC && workerId == 0) {
-                Rust.wasm_gui.then(function(wasmgui) {
+                Rust.wasm_gui.then(function(slime_seed_finder_web) {
                     let seltextarea = document.getElementById(
                         "selection_output"
                     );
                     let seedInfo = JSON.parse(seltextarea.value);
                     let minecraft_version = seedInfo.version;
-                    let r = wasmgui.generate_rivers_candidate(
+                    let r = slime_seed_finder_web.generate_rivers_candidate(
                         JSON.stringify({
                             version: minecraft_version,
                             seed: "" + startedX,
@@ -394,9 +394,9 @@ function runGui() {
 // Count candidates
 function countCandidates() {
     Rust.wasm_gui.then(
-        function(wasmgui) {
+        function(slime_seed_finder_web) {
             let outta = document.getElementById("num_candidates");
-            outta.value = wasmgui.count_rivers(
+            outta.value = slime_seed_finder_web.count_rivers(
                 JSON.stringify({ seedInfo: seedInfo })
             );
         },
@@ -407,8 +407,10 @@ function countCandidates() {
 }
 
 function drawVoronoi() {
-    Rust.wasm_gui.then(function(wasmgui) {
-        let r = wasmgui.draw_rivers(JSON.stringify({ seedInfo: seedInfo }));
+    Rust.wasm_gui.then(function(slime_seed_finder_web) {
+        let r = slime_seed_finder_web.draw_rivers(
+            JSON.stringify({ seedInfo: seedInfo })
+        );
         drawMapToCanvas(
             document.getElementById("mapLayer43"),
             r.l43,

static/rivers_worker.js
@@ -3,9 +3,9 @@ importScripts("wasm_gui.js");
 onmessage = function(e) {
     console.log("Message received from main script");
     Rust.wasm_gui.then(
-        function(wasmgui) {
+        function(slime_seed_finder_web) {
             console.log("Calling Rust code...");
-            let workerResult = wasmgui.river_seed_finder(e.data);
+            let workerResult = slime_seed_finder_web.river_seed_finder(e.data);
             console.log("Posting message back to main script");
             postMessage(workerResult);
         },

static/slime_map.html
@@ -201,7 +201,7 @@
                         });
                 } else {
                     return Rust.wasm_gui.then(
-                        function(wasmgui) {
+                        function(slime_seed_finder_web) {
                             let ws = document.getElementById("worldSeed");
                             // Since js doesnt support 64 bit ints, we just pass
                             // the string array to the Rust code, it will be parsed there
@@ -215,7 +215,7 @@
                             console.log(
                                 "Generating fragment: " + fx + ", " + fy
                             );
-                            let rvec = wasmgui.generate_fragment_slime_map(
+                            let rvec = slime_seed_finder_web.generate_fragment_slime_map(
                                 fx,
                                 fy,
                                 seed,
@@ -259,8 +259,8 @@
                 // Oh, right, the seed is 64 bits
 
                 Rust.wasm_gui.then(
-                    function(wasmgui) {
-                        let new_value = wasmgui.gen_test_seed_base_n_bits(
+                    function(slime_seed_finder_web) {
+                        let new_value = slime_seed_finder_web.gen_test_seed_base_n_bits(
                             base.value,
                             n.value,
                             bits.value

static/treasure.js
@@ -347,13 +347,13 @@ function runWorkers(numWorkers, seedInfo) {
             );
             // Draw sample image to canvas
             if (l42AreaC && workerId == 0) {
-                Rust.wasm_gui.then(function(wasmgui) {
+                Rust.wasm_gui.then(function(slime_seed_finder_web) {
                     let seltextarea = document.getElementById(
                         "selection_output"
                     );
                     let seedInfo = JSON.parse(seltextarea.value);
                     let minecraft_version = seedInfo.version;
-                    let r = wasmgui.generate_rivers_candidate(
+                    let r = slime_seed_finder_web.generate_rivers_candidate(
                         JSON.stringify({
                             version: minecraft_version,
                             seed: "" + startedX,
@@ -404,9 +404,9 @@ function runGui() {
 // Count candidates
 function countCandidates() {
     Rust.wasm_gui.then(
-        function(wasmgui) {
+        function(slime_seed_finder_web) {
             let outta = document.getElementById("num_candidates");
-            outta.value = wasmgui.count_rivers(
+            outta.value = slime_seed_finder_web.count_rivers(
                 JSON.stringify({ seedInfo: seedInfo })
             );
         },
@@ -417,8 +417,10 @@ function countCandidates() {
 }
 
 function drawVoronoi() {
-    Rust.wasm_gui.then(function(wasmgui) {
-        let r = wasmgui.draw_rivers(JSON.stringify({ seedInfo: seedInfo }));
+    Rust.wasm_gui.then(function(slime_seed_finder_web) {
+        let r = slime_seed_finder_web.draw_rivers(
+            JSON.stringify({ seedInfo: seedInfo })
+        );
         drawMapToCanvas(
             document.getElementById("mapLayer43"),
             r.l43,
@@ -477,9 +479,9 @@ function convertIntsToColor(r, g, b, a) {
 }
 
 function drawTreasureMap() {
-    Rust.wasm_gui.then(function(wasmgui) {
+    Rust.wasm_gui.then(function(slime_seed_finder_web) {
         console.log("draw_treasure_map");
-        let q = wasmgui.draw_treasure_map(
+        let q = slime_seed_finder_web.draw_treasure_map(
             JSON.stringify({ seedInfo: seedInfo })
         );
         drawMapToCanvas(document.getElementById("treasureCanvas"), q, {

static/treasure_worker.js
@@ -3,9 +3,9 @@ importScripts("wasm_gui.js");
 onmessage = function(e) {
     console.log("Message received from main script");
     Rust.wasm_gui.then(
-        function(wasmgui) {
+        function(slime_seed_finder_web) {
             console.log("Calling Rust code...");
-            let workerResult = wasmgui.draw_treasure_map(e.data);
+            let workerResult = slime_seed_finder_web.draw_treasure_map(e.data);
             console.log("Posting message back to main script");
             postMessage(workerResult);
         },

commit 07ee5f0b70f749d68e8b2b93965cd7322572d058

Fix name of wasm file after moving the code to a separate folder
Issue introduced in fed526f0dacd39ede963824a1269597690ec03a0

static/anvil.html
@@ -128,7 +128,7 @@ Found seeds will appear here.</textarea
                 >
             </div>
         </div>
-        <script type="text/javascript" src="wasm_gui.js"></script>
+        <script type="text/javascript" src="slime_seed_finder_web.js"></script>
         <script type="text/javascript" src="anvil.js"></script>
     </body>
 </html>

static/anvil.js
@@ -96,7 +96,7 @@ function runGui() {
     let seltextarea = document.getElementById("selection_output");
     if (window.Worker) {
         let maxWorkers = navigator.hardwareConcurrency || 4;
-        Rust.wasm_gui.then(function(slime_seed_finder_web) {
+        Rust.slime_seed_finder_web.then(function(slime_seed_finder_web) {
             let seedInfo_str = slime_seed_finder_web.anvil_region_to_river_seed_finder(
                 region
             );
@@ -115,7 +115,7 @@ function runGui() {
 
 // Count candidates
 function countCandidates() {
-    Rust.wasm_gui.then(
+    Rust.slime_seed_finder_web.then(
         function(slime_seed_finder_web) {
             let outta = document.getElementById("num_candidates");
             outta.value = slime_seed_finder_web.count_rivers(

static/biomes.html
@@ -99,7 +99,7 @@
         <div style="clear: both;"></div>
         <script src="common.js"></script>
         <script src="fragment_map.js"></script>
-        <script type="text/javascript" src="wasm_gui.js"></script>
+        <script type="text/javascript" src="slime_seed_finder_web.js"></script>
         <script>
             // https://codeburst.io/throttling-and-debouncing-in-javascript-646d076d0a44
             function debounced(delay, fn) {
@@ -436,7 +436,7 @@
                     ws.value = seed.toString();
                     */
                     // Ok, I'll just implement it in Rust
-                    return Rust.wasm_gui.then(
+                    return Rust.slime_seed_finder_web.then(
                         function(slime_seed_finder_web) {
                             let new_value;
                             if (add_not_sub) {
@@ -462,7 +462,7 @@
                 let ws = document.getElementById("worldSeed");
                 if (ws) {
                     // Ok, I'll just implement it in Rust
-                    return Rust.wasm_gui.then(
+                    return Rust.slime_seed_finder_web.then(
                         function(slime_seed_finder_web) {
                             let new_value = slime_seed_finder_web.similar_biome_seed(
                                 ws.value
@@ -620,7 +620,7 @@
                             return c;
                         });
                 } else {
-                    return Rust.wasm_gui.then(
+                    return Rust.slime_seed_finder_web.then(
                         function(slime_seed_finder_web) {
                             let ws = document.getElementById("worldSeed");
                             // Since js doesnt support 64 bit ints, we just pass

static/index.html
@@ -91,7 +91,7 @@ Found 64-bit seeds will appear here.</textarea
 
         <script src="common.js"></script>
         <script src="select_map.js"></script>
-        <script type="text/javascript" src="wasm_gui.js"></script>
+        <script type="text/javascript" src="slime_seed_finder_web.js"></script>
         <script>
             if (window.Worker) {
                 let myWorker = new Worker("worker.js");
@@ -109,7 +109,7 @@ Found 64-bit seeds will appear here.</textarea
                         console.log("Got message from worker");
                     };
                 } else {
-                    Rust.wasm_gui.then(
+                    Rust.slime_seed_finder_web.then(
                         function(slime_seed_finder_web) {
                             // TODO: check if a thread is already running and kill it
                             outta.value = slime_seed_finder_web.slime_seed_finder(
@@ -125,7 +125,7 @@ Found 64-bit seeds will appear here.</textarea
 
             // Count candidates
             function countCandidates() {
-                Rust.wasm_gui.then(
+                Rust.slime_seed_finder_web.then(
                     function(slime_seed_finder_web) {
                         let outta = document.getElementById("num_candidates");
                         let seltextarea = document.getElementById(
@@ -143,7 +143,7 @@ Found 64-bit seeds will appear here.</textarea
             }
 
             // Extend 48
-            Rust.wasm_gui.then(
+            Rust.slime_seed_finder_web.then(
                 function(slime_seed_finder_web) {
                     let seed48 = document.getElementById("seed48");
                     let outta = document.getElementById("output_extend48");

static/rivers.html
@@ -262,7 +262,7 @@ Found seeds will appear here.</textarea
         </div>
         <script type="text/javascript" src="rivers_common.js"></script>
         <script type="text/javascript" src="rivers_select_map.js"></script>
-        <script type="text/javascript" src="wasm_gui.js"></script>
+        <script type="text/javascript" src="slime_seed_finder_web.js"></script>
         <script type="text/javascript" src="rivers.js"></script>
     </body>
 </html>

static/rivers.js
@@ -337,7 +337,9 @@ function runWorkers(numWorkers, seedInfo) {
             );
             // Draw sample image to canvas
             if (l42AreaC && workerId == 0) {
-                Rust.wasm_gui.then(function(slime_seed_finder_web) {
+                Rust.slime_seed_finder_web.then(function(
+                    slime_seed_finder_web
+                ) {
                     let seltextarea = document.getElementById(
                         "selection_output"
                     );
@@ -393,7 +395,7 @@ function runGui() {
 
 // Count candidates
 function countCandidates() {
-    Rust.wasm_gui.then(
+    Rust.slime_seed_finder_web.then(
         function(slime_seed_finder_web) {
             let outta = document.getElementById("num_candidates");
             outta.value = slime_seed_finder_web.count_rivers(
@@ -407,7 +409,7 @@ function countCandidates() {
 }
 
 function drawVoronoi() {
-    Rust.wasm_gui.then(function(slime_seed_finder_web) {
+    Rust.slime_seed_finder_web.then(function(slime_seed_finder_web) {
         let r = slime_seed_finder_web.draw_rivers(
             JSON.stringify({ seedInfo: seedInfo })
         );

static/rivers_worker.js
@@ -1,8 +1,8 @@
-importScripts("wasm_gui.js");
+importScripts("slime_seed_finder_web.js");
 
 onmessage = function(e) {
     console.log("Message received from main script");
-    Rust.wasm_gui.then(
+    Rust.slime_seed_finder_web.then(
         function(slime_seed_finder_web) {
             console.log("Calling Rust code...");
             let workerResult = slime_seed_finder_web.river_seed_finder(e.data);

static/slime_map.html
@@ -77,7 +77,7 @@
 
         <script src="common.js"></script>
         <script src="fragment_map.js"></script>
-        <script type="text/javascript" src="wasm_gui.js"></script>
+        <script type="text/javascript" src="slime_seed_finder_web.js"></script>
         <script>
             // Hacks to make workers return promises
             // https://codeburst.io/promises-for-the-web-worker-9311b7831733
@@ -200,7 +200,7 @@
                             return c;
                         });
                 } else {
-                    return Rust.wasm_gui.then(
+                    return Rust.slime_seed_finder_web.then(
                         function(slime_seed_finder_web) {
                             let ws = document.getElementById("worldSeed");
                             // Since js doesnt support 64 bit ints, we just pass
@@ -258,7 +258,7 @@
                 let bits = document.getElementById("genSeedBits");
                 // Oh, right, the seed is 64 bits
 
-                Rust.wasm_gui.then(
+                Rust.slime_seed_finder_web.then(
                     function(slime_seed_finder_web) {
                         let new_value = slime_seed_finder_web.gen_test_seed_base_n_bits(
                             base.value,

static/treasure.html
@@ -220,7 +220,7 @@ Found seeds will appear here.</textarea
         </div>
         <script type="text/javascript" src="treasure_common.js"></script>
         <script type="text/javascript" src="treasure_select_map.js"></script>
-        <script type="text/javascript" src="wasm_gui.js"></script>
+        <script type="text/javascript" src="slime_seed_finder_web.js"></script>
         <script type="text/javascript" src="treasure.js"></script>
     </body>
 </html>

static/treasure.js
@@ -347,7 +347,9 @@ function runWorkers(numWorkers, seedInfo) {
             );
             // Draw sample image to canvas
             if (l42AreaC && workerId == 0) {
-                Rust.wasm_gui.then(function(slime_seed_finder_web) {
+                Rust.slime_seed_finder_web.then(function(
+                    slime_seed_finder_web
+                ) {
                     let seltextarea = document.getElementById(
                         "selection_output"
                     );
@@ -403,7 +405,7 @@ function runGui() {
 
 // Count candidates
 function countCandidates() {
-    Rust.wasm_gui.then(
+    Rust.slime_seed_finder_web.then(
         function(slime_seed_finder_web) {
             let outta = document.getElementById("num_candidates");
             outta.value = slime_seed_finder_web.count_rivers(
@@ -417,7 +419,7 @@ function countCandidates() {
 }
 
 function drawVoronoi() {
-    Rust.wasm_gui.then(function(slime_seed_finder_web) {
+    Rust.slime_seed_finder_web.then(function(slime_seed_finder_web) {
         let r = slime_seed_finder_web.draw_rivers(
             JSON.stringify({ seedInfo: seedInfo })
         );
@@ -479,7 +481,7 @@ function convertIntsToColor(r, g, b, a) {
 }
 
 function drawTreasureMap() {
-    Rust.wasm_gui.then(function(slime_seed_finder_web) {
+    Rust.slime_seed_finder_web.then(function(slime_seed_finder_web) {
         console.log("draw_treasure_map");
         let q = slime_seed_finder_web.draw_treasure_map(
             JSON.stringify({ seedInfo: seedInfo })

static/treasure_worker.js
@@ -1,8 +1,8 @@
-importScripts("wasm_gui.js");
+importScripts("slime_seed_finder_web.js");
 
 onmessage = function(e) {
     console.log("Message received from main script");
-    Rust.wasm_gui.then(
+    Rust.slime_seed_finder_web.then(
         function(slime_seed_finder_web) {
             console.log("Calling Rust code...");
             let workerResult = slime_seed_finder_web.draw_treasure_map(e.data);

static/worker.js
@@ -1,8 +1,8 @@
-importScripts("wasm_gui.js");
+importScripts("slime_seed_finder_web.js");
 
 onmessage = function(e) {
     console.log("Message received from main script");
-    Rust.wasm_gui.then(
+    Rust.slime_seed_finder_web.then(
         function(slime_seed_finder_web) {
             let workerResult = slime_seed_finder_web.slime_seed_finder({
                 seedInfo: e.data.seedInfo,

static/worker_b.js
@@ -1,4 +1,4 @@
-importScripts("wasm_gui.js");
+importScripts("slime_seed_finder_web.js");
 
 // Simulate lengthy calculation or an async call
 function doCalculation(slime_seed_finder_web, data, cb) {
@@ -29,7 +29,7 @@ function doCalculation(slime_seed_finder_web, data, cb) {
 self.onmessage = function(msg) {
     const { id, payload } = msg.data;
 
-    Rust.wasm_gui.then(function(slime_seed_finder_web) {
+    Rust.slime_seed_finder_web.then(function(slime_seed_finder_web) {
         doCalculation(slime_seed_finder_web, payload, function(err, result) {
             const msg = {
                 id,

static/worker_s.js
@@ -1,4 +1,4 @@
-importScripts("wasm_gui.js");
+importScripts("slime_seed_finder_web.js");
 
 // Simulate lengthy calculation or an async call
 function doCalculation(slime_seed_finder_web, data, cb) {
@@ -23,7 +23,7 @@ function doCalculation(slime_seed_finder_web, data, cb) {
 self.onmessage = function(msg) {
     const { id, payload } = msg.data;
 
-    Rust.wasm_gui.then(function(slime_seed_finder_web) {
+    Rust.slime_seed_finder_web.then(function(slime_seed_finder_web) {
         doCalculation(slime_seed_finder_web, payload, function(err, result) {
             const msg = {
                 id,