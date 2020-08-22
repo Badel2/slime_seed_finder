e1f0102d10e797535553df87982c403081b9fc19
Badel2 <2badel2@gmail.com>
2020-08-22 17:05:46 +0200

    Set travis script to failed if build_demo fails

--- ci/build_demo.sh
+++ ci/build_demo.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+# Exit script if any command returns non-zero
+set -e
+set -o pipefail
 cargo web build --target=wasm32-unknown-unknown --release -p slime_seed_finder_web
 cp target/wasm32-unknown-unknown/release/slime_seed_finder_web.* static/
 cp -rf static target/deploy
