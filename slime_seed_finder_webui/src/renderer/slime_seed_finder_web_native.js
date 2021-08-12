a4ad77c223575b9ab85761db0ac726a6b1db953c
Badel2 <2badel2@gmail.com>
2021-08-12 23:41:16 +0200

    Disable force_init logger hack
    Because it does not work on windows

--- slime_seed_finder_webui/src/renderer/slime_seed_finder_web_native.js
+++ slime_seed_finder_webui/src/renderer/slime_seed_finder_web_native.js
@@ -11,10 +11,12 @@ Rust.slime_seed_finder_web = new Promise((resolve, reject) => {
             "Loaded rust addon in slime_seed_finder_web_native.js:",
             rust_addon
         );
-        rust_addon.init(function (level, msg, f1, f2, f3) {
-            console.log("LOG");
-            //       console.log(msg, f1, f2, f3);
-        });
+        // TODO: logging disabled because node-bindgen already sets a logger and it is not possible
+        // to unset that logger and set this console.log logger
+        //rust_addon.init(function (level, msg, f1, f2, f3) {
+        //    console.log("LOG");
+        //    //       console.log(msg, f1, f2, f3);
+        //});
         resolve(rust_addon);
     }, 0);
 });
