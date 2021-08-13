6d636684c1ffd50f1deabeed50e3184af4a83f10
Badel2 <2badel2@gmail.com>
2021-08-13 02:26:01 +0200

    Revert "Disable force_init logger hack"
    This reverts commit a4ad77c223575b9ab85761db0ac726a6b1db953c.

--- slime_seed_finder_webui/src/renderer/slime_seed_finder_web_native.js
+++ slime_seed_finder_webui/src/renderer/slime_seed_finder_web_native.js
@@ -11,12 +11,10 @@ Rust.slime_seed_finder_web = new Promise((resolve, reject) => {
             "Loaded rust addon in slime_seed_finder_web_native.js:",
             rust_addon
         );
-        // TODO: logging disabled because node-bindgen already sets a logger and it is not possible
-        // to unset that logger and set this console.log logger
-        //rust_addon.init(function (level, msg, f1, f2, f3) {
-        //    console.log("LOG");
-        //    //       console.log(msg, f1, f2, f3);
-        //});
+        rust_addon.init(function (level, msg, f1, f2, f3) {
+            console.log("LOG");
+            //       console.log(msg, f1, f2, f3);
+        });
         resolve(rust_addon);
     }, 0);
 });
