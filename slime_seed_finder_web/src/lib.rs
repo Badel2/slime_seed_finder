22f41843646f5b94c329caca1e5838557934e43e
Badel2 <2badel2@gmail.com>
2021-12-24 11:35:15 +0100

    Add back panic handler to web demo
    unreachable executed errors were not helpful

--- slime_seed_finder_web/src/lib.rs
+++ slime_seed_finder_web/src/lib.rs
@@ -1,3 +1,4 @@
+use std::panic;
 use wasm_bindgen::prelude::*;
 
 pub mod wasm_gui;
@@ -5,6 +6,13 @@ pub mod wasm_gui;
 #[wasm_bindgen(start)]
 pub fn init() {
     wasm_logger::init(wasm_logger::Config::default());
+
+    // Set panic hook so we get backtrace in console
+    let next_hook = panic::take_hook();
+    panic::set_hook(Box::new(move |info| {
+        log::error!("PANIC: {}", &info.to_string());
+        next_hook(info);
+    }));
 
     // Logging
     log::debug!("Logger enabled, hello from Rust!");
